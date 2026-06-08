1820c58f5aee67e89e7508442413f07638b768eb
Paulo Cidro <paulocidro@gmail.com>
2026-06-08 18:00:59 -0300

    criando hero e iniciando o dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/layout/Container";
+
+export default async function DashboardPage() {
+  return (
+    <Container>
+      <h1>Página dashboard</h1>
+    </Container>
+  );
+}
