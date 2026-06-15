9279abd797e5f593318dac9b39b122388d089603
Paulo Cidro <paulocidro@gmail.com>
2026-06-15 14:37:08 -0300

    adicionando rota protegida pro dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,13 @@
 import Container from "@/components/layout/Container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    redirect("/");
+  }
   return (
     <Container>
       <h1>Página dashboard</h1>
